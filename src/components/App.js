aab63eabe441f2e6591a4ee9f6da529eb6132635
Brett Sartain <brettsartain@Bretts-MBP.lan>
2018-10-02 21:42:39 -0400

    Added React share buttons

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import { getPosts, showPost } from '../actions';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  EmailShareButton,
+} from 'react-share';
+
+import {
+  FacebookIcon,
+  TwitterIcon,
+  EmailIcon,
+} from 'react-share';
 
 class App extends Component {
 
@@ -24,6 +35,9 @@ class App extends Component {
           <div key={post.id} >
             <div className="devo-container" dangerouslySetInnerHTML={{__html: post.body}}></div>
             <div className="keyVerse" dangerouslySetInnerHTML={{__html: post.keyverse}}></div>
+            <FacebookShareButton url={'https://brettsartain.org/spurgeon/#/'} children={'Spurgeon'} quote={'This is a spurgeon share'}><FacebookIcon size={32} round={false} /></FacebookShareButton>
+            <TwitterShareButton url={'https://brettsartain.org/spurgeon/#/'} children={'Spurgeon'} title={'This is a spurgeon share'} via={'btsartain'} hashtags={['SpurgeonTwitter']}><TwitterIcon size={32} round={false} /></TwitterShareButton>
+            <EmailShareButton url={'https://brettsartain.org/spurgeon/#/'} children={'Spurgeon'} quote={'This is a spurgeon share'}><EmailIcon size={32} round={false} /></EmailShareButton>
           </div>
         )
       }
